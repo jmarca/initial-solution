6d807dab27e75777fa3cbfadcf7e1db053491a1d
James E. Marca <james@activimetrics.com>
2019-04-26 10:06:33 -0700

    testing
    get rid of unused code from read_csv
    
    test for read_csv
    data for testing read_csv
    
    configuration for testing to ignore google protobuf warning

---
+++ setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+import io
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+setup(
+    name='open-solver-initial-solution',
+    version='0.0.1',
+    license='Apache Software License 2.0',
+    description='A python-based front-end to Google OR-tools for the truck load problem.',
+    long_description='A python-based front-end to Google OR-tools for the truck load problem.',
+    author='James E. Marca',
+    author_email='james@activimetrics.com',
+    url='https://github.com/jmarca/initial-solution',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities',
+    ],
+    keywords=[
+        # eg: 'keyword1', 'keyword2', 'keyword3',
+    ],
+    install_requires=[
+        # eg: 'aspectlib==1.1.1', 'six>=1.7',
+    ],
+    extras_require={
+        # eg:
+        #   'rst': ['docutils>=0.11'],
+        #   ':python_version=="2.6"': ['argparse'],
+    },
+    entry_points={
+        'console_scripts': [
+            'initial-solution = initial-solution.cli:main',
+        ]
+    },
+)
---
+++ test/test_read_csv.py
@@ -0,0 +1,30 @@
+import read_csv as reader
+
+def test_load_demand():
+    d = reader.load_demand_from_csv('test/data/demand.csv')
+    assert (d.early.min() > 0)
+    assert (d.early.max() < 10000)
+    assert (d.late.min() > 0)
+    assert (d.late.max() < 10000)
+
+
+def test_load_distance_matrix():
+    matrix = reader.load_matrix_from_csv('test/data/matrix.csv')
+    assert(matrix.ndim == 2)
+    assert(matrix.size == 10*10)
+    assert(matrix.loc[0,0] == 0)
+    assert(matrix.loc[0,1] == 1269)
+    assert(matrix.loc[1,0] == 1275)
+    assert(matrix.loc[0].max() == 1842)
+    assert(matrix.loc[:,0].max() == 1857)
+
+
+
+def test_create_time_matrix():
+    matrix = reader.load_matrix_from_csv('test/data/matrix.csv')
+    hours_matrix = reader.travel_time(60,matrix)
+    assert (hours_matrix.loc[0,0] == 0)
+    assert (hours_matrix.loc[0,1] == 21.15)
+    minutes_matrix = reader.travel_time(1,matrix)
+    assert (minutes_matrix.loc[0,0] == 0)
+    assert (minutes_matrix.loc[0,1] == 1269.0) # a mile a minute
