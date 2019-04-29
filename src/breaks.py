aff90ff6fffc102c9923ccc6d2e49d57a871c8d1
James E. Marca <james@activimetrics.com>
2019-04-29 11:51:29 -0700

    create augmented travel matrix, with tests

--- src/breaks.py
+++ src/breaks.py
@@ -1,6 +1,8 @@
 """ utilities for making break nodes between normal nodes """
-# import pandas as pd
+import pandas as pd
+import numpy as np
 import math
+import sys
 
 def make_nodes(O,D,travel_time,starting_node):
     """starting with O, ending with D, make a dummy node every hour
@@ -45,3 +47,69 @@ def make_nodes(O,D,travel_time,starting_node):
     # not symmetric, but rather, directional.  Opposite way is impossible
     # so those values are NaN and easily set to infinity
     return new_times
+
+# function for applying to demand
+# use closure for access to global
+#
+# FIXME this is ugly, and will not parallelize well, and therefore may
+# not actually do what I want in some cases.  The problem is figuring
+# out in advance where to start numbering the new nodes for each
+# record, OR figuring out after the fact how to renumber the new nodes
+# so that they make sense.  The approach below is setting the
+# numbering deliberately.  A better approach would allow each
+# invocation to be independent, maybe storing the results in a
+# hashmap.
+#
+
+def break_generator(travel_times):
+    min_start = len(travel_times[0]) + 1
+    def gen_breaks(record):
+        tt = travel_times.loc[record.from_node,record.to_node]
+        new_node_start = max(min_start,record.destination + 1)
+        new_times = make_nodes(record.origin,
+                               record.destination,
+                               tt,
+                               new_node_start)
+        # print(new_times)
+        return new_times # len(new_times) - 2
+
+    return gen_breaks
+
+def aggregate_time_matrix(travel_time,newtimes):
+    """combine current time matrix with list of new times from gen_breaks, above"""
+
+    print(travel_time)
+    max_new_node = 0
+    offset = len(travel_time.loc[0])
+    for nt in newtimes:
+        new_df = pd.DataFrame(data=nt)
+        new_df = new_df.fillna(sys.maxsize)
+
+        # need to adjust the dataframe
+        # first the columns
+        adjustment = [max_new_node  for i in range(0,len(new_df.columns))]
+        adjustment[0] = 0
+        adjustment[1] = 0
+        new_df.columns = [i + adj for (i,adj) in zip(new_df.columns,adjustment)]
+
+        # then the rows (index)
+        new_df.index = [i + adj for (i,adj) in zip(new_df.index,adjustment)]
+        new_df = new_df.reindex()
+
+        new_cols = [i for i in range(2,len(new_df))]
+        old_cols = [0] # don't bother with destination, as it cannot get anywhere
+        max_new_node = new_df.columns.max() - offset
+
+        # first append the new destinations for existing columns
+        travel_time = travel_time.append(new_df.iloc[new_cols,old_cols])
+
+        # then join in the new rows and columns
+        reduced_df = new_df.iloc[new_cols,new_cols]
+        reduced_df = reduced_df.reindex()
+        travel_time = travel_time.join(reduced_df
+                                       ,how='outer'
+        )
+
+    # now replace NaN with infinity
+    travel_time.fillna(sys.maxsize)
+    return travel_time
--- test/test_breaks.py
+++ test/test_breaks.py
@@ -1,4 +1,7 @@
-import breaks as b
+import breaks as B
+import demand as D
+import read_csv as reader
+import math
 
 def test_make_nodes():
 
@@ -7,28 +10,66 @@ def test_make_nodes():
     destination = 20
     travel_time = 60
     starting_node = 30
-    new_tt_matrix = b.make_nodes(origin,destination,travel_time,starting_node)
+    new_tt_matrix = B.make_nodes(origin,destination,travel_time,starting_node)
     assert len(new_tt_matrix) == 2
     assert new_tt_matrix == {10:{10:0,20:60},20:{20:0}}
 
     # one new node at 61 minutes
     travel_time = 61
-    new_tt_matrix = b.make_nodes(origin,destination,travel_time,starting_node)
+    new_tt_matrix = B.make_nodes(origin,destination,travel_time,starting_node)
     assert len(new_tt_matrix) == 3
     assert new_tt_matrix == {10:{10:0,20:61,30:60},20:{20:0},30:{30:0,20:1}}
 
     # one new node at 120 minutes
     travel_time = 120
-    new_tt_matrix = b.make_nodes(origin,destination,travel_time,starting_node)
+    new_tt_matrix = B.make_nodes(origin,destination,travel_time,starting_node)
     assert len(new_tt_matrix) == 3
     assert new_tt_matrix == {10:{10:0,20:120,30:60},20:{20:0},30:{30:0,20:60}}
 
     # two new nodes at 121 minutes
     travel_time = 121
-    new_tt_matrix = b.make_nodes(origin,destination,travel_time,starting_node)
+    new_tt_matrix = B.make_nodes(origin,destination,travel_time,starting_node)
     assert len(new_tt_matrix) == 4 # O + D + 2 new nodes
     assert new_tt_matrix == {10:{10:0,20:121,30:60,31:120},
                              20:{20:0},
                              30:{30:0,31:60,20:61},
                              31:{31:0,20:1}
     }
+
+    # now test the break generator
+    # first read in the test demand
+    horizon = 10000
+    d     = D.Demand('test/data/demand.csv',horizon)
+    # read in travel time
+    m = reader.load_matrix_from_csv('test/data/matrix.csv')
+    # assume a mile a minute (60mph), so m in miles === m in minutes
+
+    gb = B.break_generator(m)
+    # apply to demand
+    newtimes = d.demand.apply(gb,axis=1,result_type='reduce')
+    print(newtimes)
+    assert len(newtimes) == len(d.demand)
+    # check each row for reasonableness
+    count_new_nodes = 0
+    count_nodes = 0
+    for idx in range(0,len(newtimes)):
+        origin = d.demand.loc[idx,'origin']
+        dest   = d.demand.loc[idx,'destination']
+        nt = newtimes[idx]
+        assert origin in nt.keys()
+        assert dest in nt.keys()
+        # check length
+        tt = m.loc[d.demand.loc[idx,'from_node'],d.demand.loc[idx,'to_node']]
+        expected_num_nodes = math.floor(tt/60)
+        if tt % 60 == 0:
+            expected_num_nodes -= 1
+        assert len(nt) == expected_num_nodes + 2
+        count_new_nodes += (len(nt) - 2)
+        count_nodes += len(nt)
+
+    # now test generating new travel time matrix
+    new_tt = B.aggregate_time_matrix(m,newtimes)
+    assert len(new_tt) == count_nodes
+
+    assert new_tt.ndim == 2
+    assert len(new_tt[0]) == count_nodes
