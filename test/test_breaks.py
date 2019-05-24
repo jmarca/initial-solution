ddf91fba34b94eb055de9b2fc6dc3ee334272d91
James E. Marca <james@activimetrics.com>
2019-05-24 11:38:46 -0700

    updating breaks and demand testing
    move or delete old, unused code
    remove old run_solver executable
    
    improve test coverage

--- test/test_breaks.py
+++ test/test_breaks.py
@@ -54,137 +54,194 @@ def test_make_nodes():
                              31:{31:0,20:1}
     }
 
-    # now test the break generator
+    # test split_links
+
+def test_split_links():
+
+    # split links in half, insert a new node
+    origin = 10
+    destination = 20
+    travel_time = 60
+    starting_node = 30
+    new_tt_matrix = B.split_links(origin,destination,travel_time,starting_node)
+    assert len(new_tt_matrix) == 3
+    assert new_tt_matrix == {10:{10:0,20:60,30:30},20:{20:0},30:{20:30,30:0}}
+
+    # check that pandas reads that in correctly
+    df = pd.DataFrame.from_dict(new_tt_matrix,orient='index')
+    assert df.loc[10,10] == 0
+    assert df.loc[10,20] == 60
+    assert df.loc[10,30] == 30
+    assert df.loc[30,20] == 30
+    assert np.isnan(df.loc[20,30])
+
+
+def test_split_links_break_nodes():
+
+    # split up a link as needed for breaks
+    origin = 10
+    destination = 20
+    travel_time = 61
+    starting_node = 30
+    break_time = 123
+    reset_time = 456
+    pair = B.split_links_break_nodes(origin,destination,
+                                     travel_time,
+                                     starting_node,
+                                     break_time=break_time,
+                                     reset_time=reset_time)
+    assert len(pair) == 2
+    new_times = pair[0]
+    # times should be a numpy array
+    assert len(new_times) == 3
+    assert len(new_times[0]) == 3
+    assert new_times[0][0] == origin
+    assert new_times[0][1] == starting_node
+    assert int(new_times[0][2]) == 30
+
+    assert new_times[1][0] == starting_node
+    assert new_times[1][1] == starting_node
+    assert int(new_times[1][2]) == 0
+
+    assert new_times[2][0] == starting_node
+    assert new_times[2][1] == destination
+    assert int(new_times[2][2]) == 31
+
+    # now inspect the break node object
+    break_node = pair[1]
+    assert break_node.origin == origin
+    assert break_node.destination == destination
+    assert break_node.node == starting_node
+    assert break_node.break_time == break_time
+    assert break_node.accumulator_reset == reset_time
+    assert break_node.drive_time_restore() == -reset_time
+    assert break_node.tt_o == 30
+    assert break_node.tt_d == 31
+
+def test_break_node_splitter():
+
+    origin = 10
+    destination = 20
+    travel_time = 61
+    starting_node = 30
+    triple = B.break_node_splitter(origin,destination,travel_time,starting_node)
+    assert len(triple)==3
+    new_times = triple[0]
+    assert len(new_times) == 7 # 3 for long break, 4 for short break
+    assert new_times[0][0] == origin
+    assert new_times[0][1] == starting_node
+    assert int(new_times[0][2]) == 30
+
+    assert new_times[1][0] == starting_node
+    assert new_times[1][1] == starting_node
+    assert int(new_times[1][2]) == 0
+
+    assert new_times[2][0] == starting_node
+    assert new_times[2][1] == destination
+    assert int(new_times[2][2]) == 31
+
+    # now the 8 hr break node
+    assert new_times[3][0] == origin
+    assert new_times[3][1] == starting_node+1
+    assert int(new_times[3][2]) == 15
+
+    assert new_times[4][0] == starting_node+1
+    assert new_times[4][1] == starting_node+1
+    assert int(new_times[4][2]) == 0
+
+    assert new_times[5][0] == starting_node+1
+    assert new_times[5][1] == starting_node
+    assert int(new_times[5][2]) == 15
+
+    # and the link to the destination too
+    assert new_times[6][0] == starting_node+1
+    assert new_times[6][1] == destination
+    assert int(new_times[6][2]) == 46
+
+
+    break_nodes = triple[1]
+    assert len(break_nodes)==2 # one long break, one short break
+
+    next_new_node = triple[2]
+    assert next_new_node == starting_node + 2
+
+def test_split_break_node():
+
+    origin = 10
+    destination = 20
+    travel_time = 61
+    starting_node = 30
+    triple = B.break_node_splitter(origin,destination,travel_time,starting_node)
+    assert len(triple)==3
+    new_times = triple[0]
+    assert len(new_times) == 7 # 3 for long break, 4 for short break
+    assert new_times[0][0] == origin
+    assert new_times[0][1] == starting_node
+    assert int(new_times[0][2]) == 30
+
+    assert new_times[1][0] == starting_node
+    assert new_times[1][1] == starting_node
+    assert int(new_times[1][2]) == 0
+
+    assert new_times[2][0] == starting_node
+    assert new_times[2][1] == destination
+    assert int(new_times[2][2]) == 31
+
+    # now the 8 hr break node
+    assert new_times[3][0] == origin
+    assert new_times[3][1] == starting_node+1
+    assert int(new_times[3][2]) == 15
+
+    assert new_times[4][0] == starting_node+1
+    assert new_times[4][1] == starting_node+1
+    assert int(new_times[4][2]) == 0
+
+    assert new_times[5][0] == starting_node+1
+    assert new_times[5][1] == starting_node
+    assert int(new_times[5][2]) == 15
+
+    # and the link to the destination too
+    assert new_times[6][0] == starting_node+1
+    assert new_times[6][1] == destination
+    assert int(new_times[6][2]) == 46
+
+
+    break_nodes = triple[1]
+    assert len(break_nodes)==2 # one long break, one short break
+
+    next_new_node = triple[2]
+    assert next_new_node == starting_node + 2
+
+def test_insert_nodes_for_breaks():
+
+    # now test the break generator inside demand
     # first read in the test demand
     horizon = 10000
-    d     = D.Demand('test/data/demand.csv',horizon)
-    # read in travel time
     m = reader.load_matrix_from_csv('test/data/matrix.csv')
+    d     = D.Demand('test/data/demand.csv',m,horizon,debug=True)
+    # read in travel time
     # assume a mile a minute (60mph), so m in miles === m in minutes
     # convert to solver space
     m = d.generate_solver_space_matrix(m)
-    gb = B.break_generator(m)
-    # apply to demand
-    newtimes = d.demand.apply(gb,axis=1,result_type='reduce')
-    print(newtimes)
-    assert len(newtimes) == len(d.demand)
+    # this should drastically trim travel matrix, since only one
+    # demand is feasible before 10000
+    assert len(m) == 3
+    newtimes = d.insert_nodes_for_breaks(m)
+    assert len(newtimes) > len(m)
+    # expect to have added 4 nodes between 0-1, 5 nodes between 1-2, 2
+    # nodes 2-0 plus original 2
+    assert len(newtimes) == 11 + 3
+
     # check each row for reasonableness
     count_new_nodes = 0
     count_nodes = 0
-    for idx in range(0,len(newtimes)):
+    feasible_index = d.demand.feasible
+    for idx in d.demand.index[feasible_index]:
+        print(idx)
         origin = d.demand.loc[idx,'origin']
         dest   = d.demand.loc[idx,'destination']
-        nt = newtimes[idx]
         print(origin,dest,idx)
-        assert origin in nt.keys()
-        assert dest in nt.keys()
-        if idx==0:
-            assert 11 in nt[origin].keys()
-        # check length
-        tt = m.loc[origin,dest]
-        expected_num_nodes = math.floor(tt/60)
-        if tt % 60 == 0:
-            expected_num_nodes -= 1
-        assert len(nt) == expected_num_nodes + 2
-        count_new_nodes += (len(nt) - 2)
-        count_nodes += len(nt)
-        print(idx,origin,dest,tt,count_nodes,count_new_nodes)
-    # now test generating new travel time matrix
-    print(newtimes)
-    new_tt = B.aggregate_time_matrix(m,newtimes)
-    print(new_tt.loc[[0,1,6,11,12,13],[0,1,6,11,12,13]])
-    assert len(new_tt) == count_nodes + 1 # account for depot node too
-
-    assert new_tt.loc[11,12] == 60
-    assert new_tt.loc[d.demand.loc[0,'origin'],11] == 60
-    assert new_tt.loc[d.demand.loc[0,'origin'],12] == 120
-    assert new_tt.ndim == 2
-    assert len(new_tt[0]) == count_nodes + 1
-
-
-
-    # now test the break generator with specified timelength
-    # first read in the test demand
-    horizon = 10000
-    d     = D.Demand('test/data/demand.csv',horizon)
-    timelength = 600
-    # read in travel time
-    m = reader.load_matrix_from_csv('test/data/matrix.csv')
-    # assume a mile a minute (60mph), so m in miles === m in minutes
-    # convert to solver space
-    m = d.generate_solver_space_matrix(m)
-    gb = B.break_generator(m,600)
-    # apply to demand
-    newtimes = d.demand.apply(gb,axis=1,result_type='reduce')
-    print(newtimes)
-    assert len(newtimes) == len(d.demand)
-    # check each row for reasonableness
-    count_new_nodes = 0
-    count_nodes = 0
-    for idx in range(0,len(newtimes)):
-        origin = d.demand.loc[idx,'origin']
-        dest   = d.demand.loc[idx,'destination']
-        nt = newtimes[idx]
-        print(origin,dest,idx)
-        assert origin in nt.keys()
-        assert dest in nt.keys()
-        if idx==0:
-            assert 11 in nt[origin].keys()
-        # check length
-        tt = m.loc[origin,dest]
-        expected_num_nodes = math.floor(tt/600)
-        if tt % 600 == 0:
-            expected_num_nodes -= 1
-        assert len(nt) == expected_num_nodes + 2
-        count_new_nodes += (len(nt) - 2)
-        count_nodes += len(nt)
-        print(idx,origin,dest,tt,count_nodes,count_new_nodes)
-    # now test generating new travel time matrix
-    print(newtimes)
-    new_tt = B.aggregate_time_matrix(m,newtimes)
-    print(new_tt.loc[[0,2,7,11,12,13],[0,2,7,11,12,13,14]])
-    assert len(new_tt) == count_nodes + 1 # account for depot node too
-
-    assert new_tt.loc[12,13] == 600
-    assert new_tt.loc[d.demand.loc[1,'origin'],12] == 600
-    assert new_tt.loc[d.demand.loc[1,'origin'],13] == 1200
-    assert new_tt.ndim == 2
-    assert len(new_tt[0]) == count_nodes + 1
-
-    # now test that I can do that for all destinations+depot to all
-    # origins+depot and append some more
-    moretimes = []
-    destinations_idx = [idx for idx in d.destinations.index]
-    destinations_idx.append(0) # tack on the depot node
-    origins_idx = [idx for idx in d.origins.index]
-    origins_idx.append(0) # tack on the depot node
-    new_node = len(new_tt)+1
-    for didx in destinations_idx:
-        for oidx in origins_idx:
-            if oidx == didx:
-                # depot to depot is silly
-                continue
-            tt = m.loc[didx,oidx]
-            if (not np.isnan(tt)) and  tt > 60:
-                print('call with',didx,oidx,tt,new_node)
-                new_times = B.make_nodes(didx,oidx,tt,new_node)
-
-                # test results
-                assert oidx in new_times.keys()
-                assert didx in new_times.keys()
-                # check length
-                expected_num_nodes = math.floor(tt/60)
-                if tt % 60 == 0:
-                    expected_num_nodes -= 1
-                assert len(new_times) == expected_num_nodes + 2
-                count_new_nodes += (len(new_times) - 2)
-                count_nodes += len(new_times) - 2 # need -2 this time, as repeating O, D
-                moretimes.append(new_times)
-
-    new_tt = B.aggregate_time_matrix(new_tt,moretimes)
-    assert len(new_tt) == count_nodes + 1
-    assert new_tt.ndim == 2
-    assert len(new_tt[0]) == count_nodes + 1
-    assert 10 in new_tt.index
-    assert 11 in new_tt.index
-    assert 12 in new_tt.index
+        # 5 nodes link to origin---depot plus 4 break nodes
+        assert len(newtimes.loc[:,origin].index[newtimes.loc[:,origin].notna()]) == 5
+        # 6 nodes link to destination---origin plus 5 break nodes
+        assert len(newtimes.loc[:,dest].index[newtimes.loc[:,dest].notna()]) == 6
